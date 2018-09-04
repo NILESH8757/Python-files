54f3153f79d5a201fc37ac656b0f3774a6456f86
Nilesh Kumar <nilesh8757@gmail.com>
2018-09-04 16:20:13 +0530

    Update python_threads.py

--- python_threads.py
+++ python_threads.py
@@ -2,18 +2,18 @@ import threading
 
 cnt=0
 
-def bang():
+def warning():
     global cnt
     print("Your are being watched!")
     if cnt < 5:
         cnt+=1
-        threading.Timer(5.0, bang).start()
+        threading.Timer(5.0, warning).start()
     else:
         print("Be careful!")
     
     
 def main():
-    bang()
+    warning()
     return 0
 
 if __name__ == '__main__':
